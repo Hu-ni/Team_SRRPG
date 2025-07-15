32f9061c7236a22b116828788070881e30179583
bABCDd <rlatkdrbs6845@gmail.com>
2025-07-15 14:09:55 +0900

    Player 생성자 변경

--- Team_SRRPG/Model/Player.cs
+++ Team_SRRPG/Model/Player.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Team_SRRPG.Service;
 
 namespace Team_SRRPG.Model
 {
@@ -13,10 +15,13 @@ namespace Team_SRRPG.Model
         public int Level { get; set; } // 플레이어 레벨
         public int Experience { get; set; } // 플레이어 경험치
         public int Health { get; set; } // 플레이어 체력
+        public int Attack { get; set; } // 플레이어 공격력
+        public int Defense { get; set; } // 플레이어 방어력
         public int Mana { get; set; } // 플레이어 마나
         public int Gold { get; set; } // 플레이어 소지 금액
         public int Luck { get; set; } // 플레이어 행운
-        public Player(string name, string job, int level = 1, int exp = 0, int health = 100, int mana = 50, int gold = 1000, int luck = 0)
+        public Player(string name, string job, int level = 1, int exp = 0, int health = 100, 
+            int mana = 50, int gold = 1000, int luck = 0, int attack = 0, int defense = 0)
         {
             Name = name;
             Job = job;
@@ -26,6 +31,16 @@ namespace Team_SRRPG.Model
             Mana = mana;
             Gold = gold;
             Luck = luck;
+            Attack = attack;
+            Defense = defense;
+        }
+        public int GetTotalAttack(Inventory inventory)
+        {
+            return Attack + inventory.GetTotalAttackBonus();
+        }
+        public int GetTotalDefense(Inventory inventory)
+        {
+            return Defense + inventory.GetTotalDefenseBonus();
         }
     }
 }
