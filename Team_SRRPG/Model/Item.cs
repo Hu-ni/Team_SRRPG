898701013c62d11a1c9db367980f8485cf10de8b
bABCDd <rlatkdrbs6845@gmail.com>
2025-07-15 14:33:38 +0900

    Item 생성자 변경

--- Team_SRRPG/Model/Item.cs
+++ Team_SRRPG/Model/Item.cs
@@ -11,21 +11,13 @@ namespace Team_SRRPG.Model
         public int Id { get; private set; }
         public string Name { get; private set; }
         public string Description { get; private set; }
-        public int Attack { get; private set; }
-        public int Defense { get; private set; }
-        public int Health { get; private set; }
         public int Cost { get; private set; }
-        public int Luck { get; private set; }
-        public Item(int id, string name, string description, int attack = 0, int defense = 0, int health = 0, int cost = 0, int luck = 0)
+        public Item(int id, string name, string description, int cost = 0)
         {
             Id = id;
             Name = name;
             Description = description;
-            Attack = attack;
-            Defense = defense;
-            Health = health;
             Cost = cost;
-            Luck = luck;
         }
     }
 }
