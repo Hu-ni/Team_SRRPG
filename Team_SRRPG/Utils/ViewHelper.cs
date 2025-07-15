86cf7b036a85d3e0a476afae4a70d8ba4f1e3b59
ParkHun <hundl5789@gmail.com>
2025-07-15 18:51:17 +0900

    [Refactor] ViewHelper 위치 이동

---
+++ Team_SRRPG/Utils/ViewHelper.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Team_SRRPG.Utils
+{
+    public static class ViewHelper
+    {
+
+        public static void PrintTitle(string title)
+        {
+            Console.WriteLine();
+            Console.WriteLine("====================================");
+            Console.WriteLine($" {title}");
+            Console.WriteLine("====================================");
+        }
+
+        public static void PrintNotification(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"[알림] {message}");
+            Console.ResetColor();
+        }
+
+        public static void PrintDivider()
+        {
+            Console.WriteLine("------------------------------------");
+        }
+
+        public static void PrintInvalidInput()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("잘못된 입력입니다.");
+            Console.ResetColor();
+        }
+        public static void SlowWrite(string text, int delay = 40)
+        {
+            foreach (char c in text)
+            {
+                Console.Write(c);
+                Thread.Sleep(delay);
+            }
+        }
+
+        public static void SlowWriteCenter(string text, int delay = 40)
+        {
+            int width = Console.WindowWidth;
+
+            int textWidth = 0;
+            foreach (char c in text)
+            {
+                if (c >= 0xAC00 && c <= 0xD7A3)
+                    textWidth += 2;
+                else
+                    textWidth += 1;
+            }
+
+            int padding = (width - textWidth) / 2;
+            padding = Math.Max(padding, 0);
+
+            Console.Write(new string(' ', padding));
+
+            foreach (char c in text)
+            {
+                Console.Write(c);
+                Thread.Sleep(delay);
+            }
+            //Console.WriteLine();
+        }
+
+        public static void PrintInput()
+        {
+            Console.WriteLine();
+            Console.WriteLine("원하시는 행동을 입력해주세요.");
+            Console.Write(">>");
+        }
+
+        public static void RenderOptions(List<string> options, int selectedIndex)
+        {
+            for (int i = 0; i < options.Count; i++)
+            {
+                if (i == selectedIndex)
+                    Console.WriteLine($"> {options[i]}");
+                else
+                    Console.WriteLine($"  {options[i]}");
+            }
+        }
+    }
+
+}
