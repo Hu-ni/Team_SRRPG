216f448f8593e63ab03c727539cad27a941590b6
ParkHun <hundl5789@gmail.com>
2025-07-15 11:37:12 +0900

    [Style] GameContext Setter 접근자 수정

--- Team_SRRPG/Service/GameContext.cs
+++ Team_SRRPG/Service/GameContext.cs
@@ -10,9 +10,17 @@ namespace Team_SRRPG.Service
     // 공용 데이터 
     public class GameContext : Singleton<GameContext>
     {
-        public Player Player { get; set; }
-        public List<Item> items { get; set; }       // 공용 아이템, 인벤토리 아님!!
-        public List<SceneData> sceneData { get; set; }
+        public Player Player { get; private set; }
+        public Dictionary<int, Item> Items { get; private set; }       // 공용 아이템, 인벤토리 아님!!
+        public Dictionary<int, SceneData> Scenes { get; private set; }
+
+        public GameContext() 
+        {
+            //Player = JsonSerializerHelper.Deserialize<Player>();
+            Items = JsonSerializerHelper.Deserialize<List<Item>>(ITEM_DATA_FILE).ToDictionary(x => x.Id);
+            Scenes = JsonSerializerHelper.Deserialize<List<SceneData>>(SCENE_DATA_FILE).ToDictionary(x => x.Id);
+
+        }
         
     }
 }
