f3c0b3ee9916bb21ed9cbd2949c3ca3379866f11
bABCDd <rlatkdrbs6845@gmail.com>
2025-07-15 16:43:31 +0900

    Item클래스에IsEquipped값과 IsSellable값 추가

--- Team_SRRPG/Model/Item.cs
+++ Team_SRRPG/Model/Item.cs
@@ -12,12 +12,16 @@ namespace Team_SRRPG.Model
         public string Name { get; private set; }
         public string Description { get; private set; }
         public int Cost { get; private set; }
+        public bool IsEquipped { get; set; } = false; // 아이템이 장착되었는지 여부
+        public bool IsSellable { get; set; } = true; // 아이템이 판매 가능한지 여부
         public Item(int id, string name, string description, int cost = 0)
         {
             Id = id;
             Name = name;
             Description = description;
             Cost = cost;
+            IsEquipped = false;
+            IsSellable = true;
         }
     }
     public class AttackItem : Item
